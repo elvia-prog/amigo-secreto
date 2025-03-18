bffdaaeb9d5b46c65d40c14ede4688a71f8b52da
ELVIA <ecvicenteg@gmail.com>
2025-03-18 18:09:38 -0500

    agregue lineas

---
+++ README.md
@@ -0,0 +1,15 @@
+# Amigo Secreto
+
+## Descripción
+"Amigo Secreto" es una aplicación que permite a un grupo de personas realizar un sorteo para asignar amigos secretos de manera fácil y divertida. Los usuarios pueden ingresar sus nombres y la aplicación se encargará de realizar el sorteo automáticamente.
+
+## Funcionalidades
+- **Agregar Nombres**: Permite a los usuarios ingresar sus nombres en la aplicación.
+- **Realizar Sorteo**: Una vez que todos los nombres han sido ingresados, la aplicación realiza el sorteo y asigna un amigo secreto a cada participante.
+- **Visualización de Resultados**: Los resultados del sorteo se muestran de manera clara y accesible para todos los usuarios.
+
+## Instalación
+1. Clona el repositorio:
+   ```bash
+   git clone https://github.com/elvia-prog/amigo-secreto.git
+   
--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo() {
+    const input = document.querySelector('#amigo');
+    const nombre = input.value.trim();
+
+    if (nombre) {
+        amigos.push(nombre);
+        actualizarLista();
+        input.value = ''; 
+    } else {
+        alert('Por favor, ingresa un nombre.');
+    }
+}
+
+function actualizarLista() {
+    const listaAmigos = document.querySelector('#listaAmigos');
+    listaAmigos.innerHTML = ''; // Limpiar la lista actual
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos para sortear.');
+        return;
+    }
+
+    const indice = Math.floor(Math.random() * amigos.length);
+    const amigoSeleccionado = amigos[indice];
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = `<li>Amigo secreto: ${amigoSeleccionado}</li>`;
+}
